82419ef1af94ec85bb322d402e818ec463214d4f
ali <aliideno2010@gmail.com>
2025-02-28 22:46:15 +0330

    init007

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from models import Student
-from flask import Flask,render_template
+from flask import Flask,render_template,redirect,url_for
 
 app = Flask(__name__)
 
@@ -9,6 +9,16 @@ def index():
     students=Student.select()
     return render_template('index.html',students=students)
 
+@app.route('/details/<int:id>')
+def details(id):
+    s=Student.get_by_id(id)
+    return render_template('details.html',s=s)
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    s=Student.get_by_id(id)
+    s.delete_instance()
+    return redirect(url_for('index'))
 
 if __name__=="__main__":
     app.run(debug=True)
